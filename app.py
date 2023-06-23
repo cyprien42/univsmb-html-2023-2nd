eee5e0a1bc31dd1f590dbf504f31c76798dce186
cyprien42 <101254398+cyprien42@users.noreply.github.com>
2023-06-23 10:48:29 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,17 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/Accueil")
+def accueil():
+    return render_template("Accueil.html")
+
+
+@app.route("/Equipe1")
+def equipe1():
+    return render_template("Equipe1.html")
+
+
+@app.route("/Planning")
+def planning():
+    return render_template("Planning.html")
